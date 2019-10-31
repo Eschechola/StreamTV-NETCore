3a68d4429216a94f9f7e242b3752e7e70f5ada99
Eschechola <lucas.eschechola@outlook.com>
2019-10-30 21:08:50 -0300

    menu responsivo FINALIZADO E ARRUMADO

--- StreamTV/StreamTV/wwwroot/js/dashboard.js
+++ StreamTV/StreamTV/wwwroot/js/dashboard.js
@@ -1,1 +1,29 @@
-﻿
+﻿function AbrirMenu() {
+    var background = document.getElementsByClassName('background-menu-responsive')[0];
+    background.style.display = 'block';
+
+    var menu = document.getElementsByClassName('mid-menu-responsive')[0];
+    menu.style.marginLeft = "0%";
+
+    var lista = document.getElementsByTagName('li');
+
+    for (var i = 0; i < lista.length; i++) {
+        lista[i].style.marginLeft = '20%';
+        lista[i].style.opacity = '1';
+    }
+}
+
+function FecharMenu() {
+    var background = document.getElementsByClassName('background-menu-responsive')[0];
+    background.style.display = 'none';
+
+    var menu = document.getElementsByClassName('mid-menu-responsive')[0];
+    menu.style.marginLeft = "-70vw";
+
+    var lista = document.getElementsByTagName('li');
+
+    for (var i = 0; i < lista.length; i++) {
+        lista[i].style.marginLeft = '-20%';
+        lista[i].style.opacity = '0';
+    }
+}
