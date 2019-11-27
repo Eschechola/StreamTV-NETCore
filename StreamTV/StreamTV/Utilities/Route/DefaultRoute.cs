e858f4f6316dfc968ff607030ce103e1449b444a
Lucas Gabriel Eschechola da Silva <37378400+Eschechola@users.noreply.github.com>
2019-11-27 17:04:10 -0300

    Arrumando rota novamente
    Arrumando rota novamente

--- StreamTV/StreamTV/Utilities/Route/DefaultRoute.cs
+++ StreamTV/StreamTV/Utilities/Route/DefaultRoute.cs
@@ -3,6 +3,6 @@
     public class DefaultRoute
     {
         //public static string Route = "http://172.25.0.30/Videos/";
-        public static string Route = "http://stream.aapm112.int//Videos/";
+        public static string Route = "http://stream.aapm112.int/Videos/";
     }
 }
