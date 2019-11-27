5363c70d4afaf31089e75ff5e052e92c8a962354
Lucas Gabriel Eschechola da Silva <37378400+Eschechola@users.noreply.github.com>
2019-11-27 17:03:51 -0300

    Arrumando rotas
    Arrumando rotas

--- StreamTV/StreamTV/Utilities/Route/DefaultRoute.cs
+++ StreamTV/StreamTV/Utilities/Route/DefaultRoute.cs
@@ -3,6 +3,6 @@
     public class DefaultRoute
     {
         //public static string Route = "http://172.25.0.30/Videos/";
-        public static string Route = "http://senaitvs.herokuapp.com/Videos/";
+        public static string Route = "http://stream.aapm112.int//Videos/";
     }
 }
