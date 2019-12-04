d145f2d2da2bc3dffe039033c4f49f1ea50a18c8
vihhcunha <vinicius2010.cunha@hotmail.com>
2019-12-04 14:45:36 -0300

    Alteraçao de porta

--- StreamAPI/StreamAPI/Program.cs
+++ StreamAPI/StreamAPI/Program.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace StreamAPI
 {
@@ -13,7 +7,10 @@ namespace StreamAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = new WebHostBuilder()
+                        .UseUrls("http://localhost:5555")
+                        .Build();
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -21,6 +18,8 @@ namespace StreamAPI
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+
+                }
+             );
     }
 }
